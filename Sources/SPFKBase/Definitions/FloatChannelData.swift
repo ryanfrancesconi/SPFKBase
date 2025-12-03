8872ed989bd9500232e8113e9c3df58fc7a108e6
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-12-03 09:54:39 -0800

    update provisioning + certificates in system

--- Sources/SPFKBase/Definitions/FloatChannelData.swift
+++ Sources/SPFKBase/Definitions/FloatChannelData.swift
@@ -1,12 +1,11 @@
+// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/SPFKBase
+
 import Foundation
 
 /// 2D array of multiple float arrays
 public typealias FloatChannelData = [[Float]]
 
 /// Initialize a new FloatChannelData. Convenience function.
-public func newFloatChannelData(channelCount: Int, length: Int) -> FloatChannelData {
-    Array(
-        repeating: Array<Float>(zeros: length),
-        count: channelCount
-    )
+public func allocateFloatChannelData(length: Int, channelCount: Int) -> FloatChannelData {
+    FloatChannelData(repeating: [Float](zeros: length), count: channelCount)
 }
