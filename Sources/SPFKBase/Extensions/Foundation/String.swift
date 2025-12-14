586b3d618fa7fe84dd7387636e10f126581ca008
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-12-14 10:09:43 -0800

    markers, tab controller, various refactors

--- Sources/SPFKBase/Extensions/Foundation/String.swift
+++ Sources/SPFKBase/Extensions/Foundation/String.swift
@@ -1,24 +1,61 @@
 // Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-utils
 
 import AudioToolbox
+import SwiftExtensions
 
 extension String {
     public static let nbsp: String = "\u{00A0}"
+}
+
+extension String {
+    /// Retains only ASCII alphanumerics, `+`, `-`, and `_`
+    public func onlyASCIIAlphanumericsPlusMinusUnderscore() -> String {
+        let okayChars = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLKMNOPQRSTUVWXYZ1234567890+-_"
+        return only(charactersIn: okayChars)
+    }
+
+    /// Translate U+00A0 NO-BREAK SPACE to standard " "
+    public func normalizedWhitespaces() -> String {
+        removing(characters: Self.nbsp)
+    }
+
+    public var abbreviated: String {
+        let input = onlyASCIIAlphanumericsPlusMinusUnderscore()
+        let uppercaseLetters = input.only(.uppercaseLetters).prefix(4).string
+
+        var output = ""
+
+        if uppercaseLetters.count > 1 {
+            output = uppercaseLetters
+
+        } else if input.contains(" ") {
+            let parts = input.components(separatedBy: " ")
+
+            for i in 0 ..< parts.count {
+                guard let letter = parts[i].first?.string else { continue }
+                output += letter
+            }
+
+        } else if input.count > 1 {
+            output = input.prefix(2).string
+        }
+
+        return output.uppercased()
+    }
 }
 
 // MARK: - Character Filters
 
 extension String {
     public var titleCased: String {
-        return self
-            .replacingOccurrences(
-                of: "([A-Z])",
-                with: " $1",
-                options: .regularExpression,
-                range: range(of: self)
-            )
-            .trimmingCharacters(in: .whitespacesAndNewlines)
-            .capitalized // If input is in llamaCase
+        replacingOccurrences(
+            of: "([A-Z])",
+            with: " $1",
+            options: .regularExpression,
+            range: range(of: self)
+        )
+        .trimmingCharacters(in: .whitespacesAndNewlines)
+        .capitalized // If input is in llamaCase
     }
 }
 
@@ -48,7 +85,8 @@ extension StringProtocol {
     /// - Omits elements starting with a null character.
     public func splitDelimited(delimiter: String = ",",
                                allowDuplicates: Bool = false,
-                               allowEmptyElements: Bool = false) -> [String] {
+                               allowEmptyElements: Bool = false) -> [String]
+    {
         let keywordArray = components(separatedBy: delimiter)
         var out: [String] = []
 
@@ -95,9 +133,7 @@ extension String {
 }
 
 extension CharacterSet {
-    public static let urlAllowed: CharacterSet = {
-        .alphanumerics.union(
-            .init(charactersIn: "-._~")
-        ) // as per RFC 3986
-    }()
+    public static let urlAllowed: CharacterSet = .alphanumerics.union(
+        .init(charactersIn: "-._~")
+    ) // as per RFC 3986
 }
